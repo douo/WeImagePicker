8b07c008266a8f8cf8456cf9edc7ca1d15a46ce0
Tiou Lims <dourokinga@gmail.com>
2015-11-19 17:30:56 +0800

    Refactor project structure

---
+++ sample/src/main/java/info/dourok/weimagepicker/sample/MainActivity.java
@@ -0,0 +1,48 @@
+package info.dourok.weimagepicker.sample;
+
+import android.content.ClipData;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import info.dourok.weimagepicker.ImagePickerActivity;
+import info.dourok.weimagepicker.image.Bucket;
+import info.dourok.weimagepicker.image.SelectedBucket;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int REQUEST_PICK = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClick(View view) {
+        Intent i = new Intent(this, ImagePickerActivity.class);
+        startActivityForResult(i, REQUEST_PICK);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PICK && resultCode == RESULT_OK) {
+            SelectedBucket bucket = Bucket.fromIntent(data, SelectedBucket.class);
+            for (long id : bucket.toArray()) {
+                Log.d("MainActivity", "bucket:" + id);
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                ClipData clipData = data.getClipData();
+                for (int i = 0; i < clipData.getItemCount(); i++) {
+                    Log.d("MainActivity", clipData.getItemAt(i).getUri().toString());
+                }
+            }
+        }
+    }
+}
