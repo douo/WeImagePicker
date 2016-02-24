56787ccf89ad912a9dbdc214e85ab9668f88b389
Tiou Lims <DouO@TioudeiMac.local>
2016-02-25 00:28:06 +0800

    Add support to M's permission

--- library/src/main/java/info/dourok/weimagepicker/ImagePickerActivity.java
+++ library/src/main/java/info/dourok/weimagepicker/ImagePickerActivity.java
@@ -1,19 +1,24 @@
 package info.dourok.weimagepicker;
 
+import android.Manifest;
 import android.content.ClipData;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.MimeTypeMap;
+import android.widget.Toast;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -24,6 +29,7 @@ import info.dourok.weimagepicker.image.SelectedBucket;
 
 public class ImagePickerActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 0x1;
     ImageContentManager mManager;
     List<Bucket> mBuckets;
     SelectedBucket mSelectedBucket;
@@ -44,6 +50,44 @@ public class ImagePickerActivity extends AppCompatActivity implements LoaderMana
         } else {
             mSelectedBucket = new SelectedBucket();
         }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            } else {
+                init();
+            }
+        } else {
+            init();
+        }
+
+    }
+
+    private void init() {
         Intent intent = getIntent();
         String action = intent.getAction();
         String type = intent.getType();
@@ -82,6 +126,27 @@ public class ImagePickerActivity extends AppCompatActivity implements LoaderMana
                 mPicker.prepared(mBuckets);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    init();
+                } else {
+                    Toast.makeText(this, "Failed to request permission", Toast.LENGTH_SHORT).show();
+                    finish();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+
+        }
     }
 
     public SelectedBucket getSelectedBucket() {
